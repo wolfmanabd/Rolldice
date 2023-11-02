c9135acf7ef8daeb4aec0d8e85ee43e7a7eb1db8
wolfman <wolfmanabd@gmail.com>
2023-11-02 19:13:38 +0100

    second commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import RollDice from './RollDice';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <RollDice /> 
+    </div>
+  );
+}
+
+export default App;
